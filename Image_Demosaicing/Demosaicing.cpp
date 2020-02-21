7755298fbff174903bf6646e6c034077e3c049e7
Ajiao <jyflyingeyebrow@gmail.com>
2020-02-21 02:47:28 -0800

    edit RM & code

--- Image_Demosaicing/Demosaicing.cpp
+++ Image_Demosaicing/Demosaicing.cpp
@@ -1,5 +1,6 @@
-// JY569HW1.cpp : Defines the entry point for the console application.
-
+// Course project of USC EE 569 Digital Image Processing
+// arrow mark pair "vvv" & "^^^": codes between these marks are code snippets from start code provided by course instructor
+// author (of codes outside arrow mark pair): Yan Jiao
 
 #include <stdio.h>
 #include <iostream>
@@ -7,13 +8,19 @@
 
 using namespace std;
 
-//
-void Extend(unsigned char *in, unsigned char *out, int extNum, int row, int col){ // row, col -> ori
-	// top
+// pre-process the input image: padding, mirror flipping the outermost pixels
+// *in -- pointer of raw image data array
+// *out -- pointer of image data array after padding
+// extNum -- padding pixel number at each edge;
+//           in bilinear method, extNum is 1;
+//           in MHC method, extNum is 2;
+// row -- row number of original input image data array
+// col -- column number of original input image data array
+void Extend(unsigned char *in, unsigned char *out, int extNum, int row, int col){
+	// padding top
 	for (int i = 1; i <= extNum; i++) {
 		for (int j = 0; j < col; j++) {
 			out[(extNum-i)*(col+2*extNum) + j + extNum]=(in[i*col + j]);
-			//out[(extNum-i)*(col+2*extNum) + j + extNum]=0;
 		}
 	}
 	// bottom
@@ -42,11 +49,14 @@ void Extend(unsigned char *in, unsigned char *out, int extNum, int row, int col)
 	}
 }
 
+
 int main(int argc, char *argv[])
 {
+  // vvv
 	// Define file pointer and variables
 	FILE *file;
 	int BytesPerPixel = 1;
+  // ^^^
 	int SizeX = 300;
 	int SizeY = 390;
   int padding_num = 2;
@@ -72,9 +82,9 @@ int main(int argc, char *argv[])
 		}
 	}
 
+  // vvv
 	// Allocate image data array
   unsigned char Imagedata[SizeX][SizeY][BytesPerPixel];
-
 	// Read image (filename specified by first argument) into image data matrix
 	if (!(file = fopen(argv[1], "rb"))) {
 		cout << "Cannot open file: " << argv[1] << endl;
@@ -82,21 +92,24 @@ int main(int argc, char *argv[])
 	}
 	fread(Imagedata, sizeof(unsigned char), SizeX*SizeY*BytesPerPixel, file);
 	fclose(file);
+  // ^^^
 
   if (strcmp(argv[3], "mhc") == 0) {
     padding_num = 4;
   }
 
-	///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
+  // initial the after padding data array
   unsigned char Temp[SizeX+padding_num][SizeY+padding_num][BytesPerPixel];
+  // initial the final output data array
   unsigned char ImagedataOut[SizeX][SizeY][BytesPerPixel*3];
-  if (strcmp(argv[3], "bi") == 0) {
-    /////////////// extend the image data array, 1px outer /////////////////////////
 
+  /////////////////////////// Bilinear /////////////////////////////////////
+  if (strcmp(argv[3], "bi") == 0) {
+
+    // extend the image data array, 1px outer
     Extend(&Imagedata[0][0][0], &Temp[0][0][0], 1, SizeX, SizeY);
 
-  	/////////////////// output image data array //////////////////////////////
-
+  	// output image data array, traverse the padded data
   	for (int i=1; i<=SizeX; i++) {
       for (int j=1; j<=SizeY; j++) {
           if ((i+j)%2 == 0) {  // already has green
@@ -124,15 +137,13 @@ int main(int argc, char *argv[])
       }
     }
   }
+  /////////////////////////// MHC /////////////////////////////////////
   else if (strcmp(argv[3], "mhc") == 0) {
-    ///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
 
   	/////////////// extend the image data array, 2px outer /////////////////////////
-
-    unsigned char Temp[SizeX+4][SizeY+4][BytesPerPixel];
   	Extend(&Imagedata[0][0][0], &Temp[0][0][0], 2, SizeX, SizeY);
-  	//----------------- output image data array
-  	int red,green,blue;
+  	//output image data array
+  	int red, green, blue;
   	for (int i=2; i<=SizeX+1; i++) {
       for (int j=2; j<=SizeY+1; j++) {
           if ((i+j)%2 == 0) {  // already has green
@@ -156,7 +167,7 @@ int main(int argc, char *argv[])
   					}
           }
           else if (i%2 == 0) { // red
-            red = Temp[i][j][0];//cout << (int)Temp[i][j][0]<<" "<<(int)Imagedata[i-2][j-2][0]<<endl;
+            red = Temp[i][j][0];
             green = ((Temp[i][j-1][0] + Temp[i][j+1][0] + Temp[i-1][j][0] + Temp[i+1][j][0]) * 2 +
   																			Temp[i][j][0] * 4 - (Temp[i][j-2][0] + Temp[i][j+2][0] + Temp[i-2][j][0] + Temp[i+2][j][0])) / 8;
             blue = ((Temp[i-1][j-1][0] + Temp[i+1][j+1][0] + Temp[i-1][j+1][0] + Temp[i+1][j-1][0]) * 2 +
@@ -170,42 +181,42 @@ int main(int argc, char *argv[])
             blue = Temp[i][j][0];
           }
 
-
-  					if (red > 255) {
-  						red = 255;
-  					}
-  					if (red < 0) {
-  						red = 0;
-  					}
-  					if (green > 255) {
-  						green = 255;
-  					}
-  					if (green < 0) {
-  						green = 0;
-  					}
-  					if (blue > 255) {
-  						blue = 255;
-  					}
-  					if (blue < 0) {
-  						blue = 0;
-  					}
-
-  					ImagedataOut[i-2][j-2][0] = red;
-  					ImagedataOut[i-2][j-2][1] = green;
-  					ImagedataOut[i-2][j-2][2] = blue;
+          // check for out bounded
+					if (red > 255) {
+						red = 255;
+					}
+					if (red < 0) {
+						red = 0;
+					}
+					if (green > 255) {
+						green = 255;
+					}
+					if (green < 0) {
+						green = 0;
+					}
+					if (blue > 255) {
+						blue = 255;
+					}
+					if (blue < 0) {
+						blue = 0;
+					}
+					ImagedataOut[i-2][j-2][0] = red;
+					ImagedataOut[i-2][j-2][1] = green;
+					ImagedataOut[i-2][j-2][2] = blue;
 
       }
     }
   }
 
+  // VVV
 	// Write image data (filename specified by second argument) from image data matrix
-
 	if (!(file = fopen(argv[2], "wb"))) {
 		cout << "Cannot open file: " << argv[2] << endl;
 		exit(1);
 	}
 	fwrite(ImagedataOut, sizeof(unsigned char), SizeX*SizeY*BytesPerPixel*3, file);
 	fclose(file);
+  // ^^^
 
   return 0;
 }
